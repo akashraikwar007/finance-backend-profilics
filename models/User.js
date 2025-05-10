230f3ce80f4ed83d896ee9cbb148eba6b5b3420a
Akash Raikwar <121796197+akashraikwar007@users.noreply.github.com>
2025-05-10 09:54:51 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -5,6 +5,7 @@ const userSchema = new mongoose.Schema(
     fullName: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
+    mobile: { type: String, required: true },
     currency: { type: String, default: "INR" },
   },
   { timestamps: true }
